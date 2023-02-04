deffcbfdc227420ee6ff8a442f399023df255c1b
KirinFilip <filip.kirin.zg1@gmail.com>
2023-02-04 15:21:49 +0100

    testReceive

--- test/MultiSigWallet.t.sol
+++ test/MultiSigWallet.t.sol
@@ -8,10 +8,25 @@ contract MultiSigWalletTest is Test {
     MultiSigWallet public multisigwallet;
 
     address alice = vm.addr(1);
+    address bob = vm.addr(2);
+    address eve = vm.addr(3);
 
-    function setUp() public {}
+    function setUp() public {
+        address[] memory _owners = new address[](3);
+        _owners[0] = alice;
+        _owners[1] = bob;
+        _owners[2] = eve;
+        multisigwallet = new MultiSigWallet(_owners, 2);
 
-    function testLog() public {
-        emit log_address(alice);
+        vm.deal(alice, 10 ether);
+        vm.deal(bob, 10 ether);
+        vm.deal(eve, 10 ether);
+    }
+
+    // transfer 1 ether via receive() and emit Deposit()
+    function testReceive() public {
+        vm.prank(alice);
+        payable(multisigwallet).transfer(1 ether);
+        assertEq(address(multisigwallet).balance, 1 ether);
     }
 }
