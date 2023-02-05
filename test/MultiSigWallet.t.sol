5c75d92013283f7b0b41067b175591ecda0aaf9b
KirinFilip <filip.kirin.zg1@gmail.com>
2023-02-05 17:42:28 +0100

    labels

--- test/MultiSigWallet.t.sol
+++ test/MultiSigWallet.t.sol
@@ -7,35 +7,56 @@ import {MultiSigWallet} from "../src/MultiSigWallet.sol";
 contract MultiSigWalletTest is Test {
     MultiSigWallet public multisigwallet;
 
-    address alice = vm.addr(1);
-    address bob = vm.addr(2);
-    address eve = vm.addr(3);
+    address alice = vm.addr(1); // in multisig
+    address bob = vm.addr(2); // in multisig
+    address carol = vm.addr(3); // in multisig
+    address dan = vm.addr(4); // outside multisig
 
+    // setup the multiSigWallet with 3 owners all with 10 ether
+    // and with 2 required number of approvals
     function setUp() public {
         address[] memory _owners = new address[](3);
         _owners[0] = alice;
         _owners[1] = bob;
-        _owners[2] = eve;
+        _owners[2] = carol;
         multisigwallet = new MultiSigWallet(_owners, 2);
+
+        vm.label(alice, "Alice");
+        vm.label(bob, "Bob");
+        vm.label(carol, "Carol");
+        vm.label(dan, "Dan");
+        vm.label(address(multisigwallet), "MultiSigWallet");
 
         vm.deal(alice, 10 ether);
         vm.deal(bob, 10 ether);
-        vm.deal(eve, 10 ether);
+        vm.deal(carol, 10 ether);
+    }
+
+    function fundWallet() internal {
+        vm.deal(address(multisigwallet), 10 ether);
     }
 
     // Does constructor work properly
     function testConstructor() public {
         assertEq(multisigwallet.owners(0), alice);
         assertEq(multisigwallet.owners(1), bob);
-        assertEq(multisigwallet.owners(2), eve);
+        assertEq(multisigwallet.owners(2), carol);
         assertEq(multisigwallet.numApprovalsRequired(), 2);
     }
 
     // transfer 1 ether via receive() and emit Deposit()
-    function testReceive(uint256 _amount) public {
+    function testFuzz_receive(uint256 _amount) public {
         vm.prank(alice);
         vm.assume(_amount <= 10 ether);
         payable(multisigwallet).transfer(_amount);
         assertEq(address(multisigwallet).balance, _amount);
     }
+
+    function test_submitTransaction() public {
+        fundWallet();
+        vm.prank(alice);
+        multisigwallet.submitTransaction(dan, 1 ether, "");
+    }
+
+    function testFail_submitTransactionNotOwner() public {}
 }
